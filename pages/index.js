e54a4b8085248eac5ecec534e238908307cbffbc
Paolo <str237@gmail.com>
2020-10-04 16:49:49 +0200

    Animate text

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import styled from '@emotion/styled'
 import { Row, Main } from "../components/layout";
 import { WIP } from "../components/svg/undraw/WIP";
+import cx from 'classnames'
 
 const WIPImage = styled(WIP)`
   min-width: 300px;
@@ -11,19 +12,24 @@ const WIPImage = styled(WIP)`
   margin-top: 100px;
 `
 
+const AnimatedRow = styled(Row)`
+  animation-delay: 0.7s;
+`
+
 export default function Home() {
+  const cssAnimated = cx('centered', 'animated',  'jackInTheBox')
   return (
     <Main>
       <Head>
         <title>Kharenzze</title>
         <link rel="icon" href="/favicon.ico"/>
       </Head>
-      <Row className="centered">
+      <AnimatedRow className={cssAnimated}>
           <h1>Paolo Raez</h1>
-      </Row>
-      <Row className="centered">
+      </AnimatedRow>
+      <AnimatedRow className={cssAnimated}>
         <span>This portfolio site is under construction...</span>
-      </Row>
+      </AnimatedRow>
       <Row className="centered">
         <WIPImage color="#1890ff"/>
       </Row>
