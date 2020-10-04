344b281d224ff78593846308c296392be901a07b
Paolo <str237@gmail.com>
2020-10-04 16:49:27 +0200

    Include styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import 'antd/dist/antd.css'
+import 'animate.css/source/_vars.css'
+import 'animate.css/source/_base.css'
+import 'animate.css/source/specials/jackInTheBox.css'
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
