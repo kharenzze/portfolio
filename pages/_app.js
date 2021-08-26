66cb8ff8107944057dbed9ac71975b8eccd8c1d4
Paolo <str237@gmail.com>
2021-08-26 10:47:43 +0200

    Remove old data

--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import 'antd/dist/antd.css'
-import 'animate.css/source/_vars.css'
-import 'animate.css/source/_base.css'
-import 'animate.css/source/specials/jackInTheBox.css'
-
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
