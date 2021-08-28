bb1f860c609a8f695c97512aa4751fe93a061bd0
Paolo <str237@gmail.com>
2021-08-28 18:26:15 +0200

    Move element on click

--- src/routes/examples/flip.svelte
+++ src/routes/examples/flip.svelte
@@ -1,19 +1,31 @@
 <script module="ts">
-  let x = 1;
-  const onClick = () => 
-    console.log('x', x)
+  let right = false;
+  const onClick = () => {
+    right = !right
+  }
 </script>
 
 <svelte:head>
 	<title>Flip</title>
 </svelte:head>
 
-<div class="box" on:click={onClick}/>
+<div class="container">
+  <div class="box" class:right on:click={onClick}/>
+</div>
 
 <style>
+  .container {
+    width: 100%;
+    height: 40px;
+    position: relative;
+  }
   .box {
     background-color: red;
     height: 40px;
     width: 40px;
+    position: absolute;
+  }
+  .right {
+    right: 0;
   }
 </style>
