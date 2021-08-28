9bf05573968b690e33457c3c5d62bb48a4716b9a
Paolo <str237@gmail.com>
2021-08-28 17:28:03 +0200

    New view for testing animations

---
+++ src/routes/examples/flip.svelte
@@ -0,0 +1,16 @@
+<script module="ts">
+  let x = 1;
+  console.log('x', x)
+</script>
+
+<svelte:head>
+	<title>Flip</title>
+</svelte:head>
+
+<p>Hola</p>
+
+<style>
+  p {
+    color: red;
+  }
+</style>
