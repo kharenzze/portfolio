29db9f285f30ebf5e80a4299554b5bf94ad5cfbe
Paolo <str237@gmail.com>
2021-08-30 12:39:04 +0200

    Animated card

--- src/routes/examples/card.svelte
+++ src/routes/examples/card.svelte
@@ -1,7 +1,46 @@
 <script lang="ts">
+  import { quadOut } from 'svelte/easing'
+  let hidden = true
+  const animation = (node, { duration = 700 }) => {
+    return {
+      duration,
+      delay: 100,
+      css: t => {
+        const et = quadOut(t)
+        console.log(et)
+
+        return `
+          opacity: ${et};
+          transform: perspective(${400 - et * 300}px) rotateY(-3deg);
+        `
+      },
+    }
+  }
+
+  const end = () => {
+    console.log('end')
+  }
+
+  setTimeout(() => {
+    hidden = false
+  }, 500)
 </script>
 
-<div class="portfolio-card">hi</div>
+{#key hidden}
+  <div class="portfolio-card card" in:animation={{ duration: 2000 }} class:hidden on:introend={end}>
+    <p>
+      Lorem fistrum qué dise usteer caballo blanco caballo negroorl torpedo a peich diodenoo a
+      gramenawer. Hasta luego Lucas.
+    </p>
+  </div>
+{/key}
 
 <style>
+  .card {
+    max-width: 300px;
+    transform: perspective(100px) rotateY(-3deg);
+  }
+  .hidden {
+    opacity: 0;
+  }
 </style>
