264c6b86830613c6c43a7f62c944214ece836da3
Paolo <str237@gmail.com>
2021-08-31 00:42:34 +0200

    Detect whether elements are displayed in screen

--- src/lib/directives/index.ts
+++ src/lib/directives/index.ts
@@ -1,3 +1,24 @@
+let global_observer: IntersectionObserver | undefined = undefined
+
+const getObserver = () => {
+  if (!global_observer) {
+    let options = {
+      threshold: 0.5,
+    }
+
+    global_observer = new IntersectionObserver((entries, observer) => {
+      for (const e of entries) {
+        if (e.isIntersecting) {
+          e.target.dispatchEvent(new CustomEvent('inscreen'))
+          observer.unobserve(e.target)
+        }
+      }
+    }, options)
+  }
+  return global_observer as IntersectionObserver
+}
+
 export const inscreen = node => {
-  console.log(node)
+  const ob = getObserver()
+  ob.observe(node)
 }
--- src/routes/examples/card.svelte
+++ src/routes/examples/card.svelte
@@ -2,30 +2,41 @@
   import { inscreen } from '../../lib/directives/index'
   let hidden = true
 
-  const end = () => {
-    console.log('end')
-  }
+  let elements = [1, 2, 3, 4, 5, 6, 7, 8, 9].map(i => ({
+    id: i,
+    hidden: true,
+  }))
 
-  setTimeout(() => {
-    hidden = false
-  }, 500)
+  const onDisplayed = element => () => {
+    console.log('displayed', element.id)
+    element.hidden = false
+    elements = elements
+  }
 </script>
 
-<div class="portfolio-card card" class:hidden on:introend={end} use:inscreen>
-  <p>
-    Lorem fistrum qué dise usteer caballo blanco caballo negroorl torpedo a peich diodenoo a
-    gramenawer. Hasta luego Lucas.
-  </p>
-</div>
+{#each elements as el}
+  <div
+    class="portfolio-card card"
+    class:hidden={el.hidden}
+    on:inscreen={onDisplayed(el)}
+    use:inscreen
+  >
+    <p>
+      Lorem fistrum qué dise usteer caballo blanco caballo negroorl torpedo a peich diodenoo a
+      gramenawer. Hasta luego Lucas.
+    </p>
+  </div>
+{/each}
 
 <style>
   .card {
+    margin: 30px;
     max-width: 300px;
     transform: perspective(100px) rotateY(-3deg);
     transition: transform 2s ease-out 1s, opacity 1s linear 0s;
   }
   .hidden {
     opacity: 0;
-    transform: none;
+    transform: perspective(100px);
   }
 </style>
